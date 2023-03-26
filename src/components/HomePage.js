cc1e70a7c1fe6a3a071edbcdd5883c9f3ec50497
mendsalbert <mendsalbert@gmail.com>
2023-03-26 23:28:18 +0000

    sustain

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -185,24 +185,24 @@ function HomePage({ projects }) {
                         <a href={`/singlepage/${index}`} className="group relative block bg-black">
                             <img
                                 alt="Developer"
-                                src={items.imageCID}
+                                src={items?.imageCID}
                                 className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
                             />
 
                             <div className="relative p-4 sm:p-6 lg:p-8">
                                 <p className="text-sm font-medium uppercase tracking-widest text-green-500">
-                                    {items.name}{' '}
+                                    {items?.name}{' '}
                                 </p>
 
                                 <p className="text-xl font-bold text-white sm:text-2xl">
-                                    {items.targetCarbonOffset.toString()} CO<sub>2</sub>e
+                                    {items?.targetCarbonOffset?.toString()} CO<sub>2</sub>e
                                 </p>
 
                                 <p className="text-sm text-white font-medium Capitalize tracking-widest">To Offset</p>
 
                                 <div className="mt-32 sm:mt-48 lg:mt-64">
                                     <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-                                        <p className="text-sm text-white">{items.description}</p>
+                                        <p className="text-sm text-white">{items?.description}</p>
                                     </div>
                                 </div>
                             </div>
