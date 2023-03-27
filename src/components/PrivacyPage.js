5592a1be82a3eeb6292e509dbe49a975a08d005b
Brilliant Kwakye <39886389+kwakyebrilliant@users.noreply.github.com>
2023-03-27 21:10:10 +0000

    Update PrivacyPage.js

--- src/components/PrivacyPage.js
+++ src/components/PrivacyPage.js
@@ -17,31 +17,21 @@ function PrivacyPage() {
             </h2>
 
             <p className="mx-auto mt-4 text-gray-500 max-w-5xl">
-                Lorem Ipsum is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a 
-                type specimen book. It has survived not only five centuries,
-                but also the leap into electronic typesetting, remaining
-                essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing 
-                    software like Aldus PageMaker including versions of 
-                    Lorem Ipsum.
+                At Sustain, we take your privacy seriously. This privacy policy explains 
+                how we collect, use, and disclose personal information in connection with 
+                our platform. By using our platform, you consent to the collection, use, and 
+                disclosure of your personal information in accordance with this policy.
             </p>
-
+        
+        
+            <h2 className="text-lg pt-20 font-extrabold text-gray-900 sm:text-5xl">
+                Information we collect
+            </h2>
             <p className="mx-auto mt-4 text-gray-500 max-w-5xl">
-                Lorem Ipsum is simply dummy text of the printing and 
-                typesetting industry. Lorem Ipsum has been the industry's 
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a 
-                type specimen book. It has survived not only five centuries,
-                but also the leap into electronic typesetting, remaining
-                essentially unchanged. It was popularised in the 1960s with
-                    the release of Letraset sheets containing Lorem Ipsum
-                    passages, and more recently with desktop publishing 
-                    software like Aldus PageMaker including versions of 
-                    Lorem Ipsum.
+               We collect personal information that you provide to us, such as your name,
+            email address, and payment information when you register for an account, donate
+            to an action, or communicate with us. We also collect information about your activities
+            on our platform, such as the actions you champion and the funds you contribute.
             </p>
 
             <p className="mx-auto mt-4 text-gray-500 max-w-5xl">
