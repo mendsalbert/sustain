016c574990b6410b36c2d145f21cead925b9597a
Brilliant Kwakye <39886389+kwakyebrilliant@users.noreply.github.com>
2023-03-29 11:03:57 +0000

    Update Faq.js

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -21,7 +21,7 @@ function FaqPage() {
                 <details className="p-6 group" open>
                     <summary className="flex items-center justify-between cursor-pointer">
                     <h5 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing?
+                        What is Sustain?
                     </h5>
 
                     <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
@@ -58,17 +58,16 @@ function FaqPage() {
                     </summary>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                    earum similique!
+                    Sustain is a platform that empowers individuals to take concrete action to reduce carbon 
+                    footprints in their communities. Users can champion actions, solicit funds, and undertake 
+                    real-world projects that help reduce greenhouse gas emissions and contribute to a more sustainable future.
                     </p>
                 </details>
 
                 <details className="p-6 group" open>
                     <summary className="flex items-center justify-between cursor-pointer">
                     <h5 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing?
+                        How does Sustain work?
                     </h5>
 
                     <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
@@ -105,17 +104,17 @@ function FaqPage() {
                     </summary>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                    earum similique!
+                    Users can sign up for Sustain and browse a variety of actions that have been championed by other members 
+                    of the community. They can read about the specific carbon offset goals of each action and decide whether 
+                    they want to contribute funds to help make it happen. Once an action has received enough funds to proceed, 
+                    the user who championed it can undertake the project and report back to the community on its progress.
                     </p>
                 </details>
 
                 <details className="p-6 group" open>
                     <summary className="flex items-center justify-between cursor-pointer">
                     <h5 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing?
+                        What kind of actions can be championed on Sustain?
                     </h5>
 
                     <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
@@ -152,17 +151,16 @@ function FaqPage() {
                     </summary>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                    earum similique!
+                    Any action that helps reduce greenhouse gas emissions and contributes to a more sustainable future can
+                    be championed on Sustain. Examples include planting trees, organizing community clean-ups, installing 
+                    energy-efficient light bulbs, and more.
                     </p>
                 </details>
 
                 <details className="p-6 group" open>
                     <summary className="flex items-center justify-between cursor-pointer">
                     <h5 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing?
+                        How is the impact of each action tracked?
                     </h5>
 
                     <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
@@ -199,17 +197,17 @@ function FaqPage() {
                     </summary>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                    earum similique!
+                    Sustain is built on the XDC blockchain, a secure and decentralized ledger that allows users to track
+                    the progress of each action and verify that their funds are being used for their intended purpose. This 
+                    level of transparency is essential to building trust and ensuring that community members can feel confident 
+                    that their contributions are making a real impact
                     </p>
                 </details>
 
                 <details className="p-6 group" open>
                     <summary className="flex items-center justify-between cursor-pointer">
                     <h5 className="text-lg font-medium text-gray-900">
-                        Lorem ipsum dolor sit amet consectetur adipisicing?
+                        How can I get involved with Sustain?
                     </h5>
 
                     <span className="relative flex-shrink-0 ml-1.5 w-5 h-5">
@@ -246,10 +244,10 @@ function FaqPage() {
                     </summary>
 
                     <p className="mt-4 leading-relaxed text-gray-700">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab hic veritatis
-                    molestias culpa in, recusandae laboriosam neque aliquid libero nesciunt
-                    voluptate dicta quo officiis explicabo consequuntur distinctio corporis
-                    earum similique!
+                    If you are passionate about protecting the planet and want to make a tangible difference in your community,
+                    we invite you to sign up for Sustain today. You can browse existing actions, champion your own actions, and 
+                    contribute funds to help make them happen. With Sustain, you can be part of a growing movement of individuals
+                    who are taking action to reduce their carbon footprint and create a more sustainable future for all.
                     </p>
                 </details>
 
